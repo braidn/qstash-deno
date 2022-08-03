1cb1dcbd2ec4eedc72336fb66b00384d9abcf717
Braden Douglass <braden.douglass@gmail.com>
2022-08-02 22:30:45 -0400

    Create Default Serve File.
    Default serve server file. Nothing super complex, just pulls in items
    for working with deno and qstash.

---
+++ lib/serve.ts
@@ -0,0 +1,23 @@
+import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import { Receiver } from "https://deno.land/x/upstash_qstash@v0.1.4/mod.ts";
+
+serve(async (req: Request) => {
+  const r = new Receiver({
+    currentSigningKey: Deno.env.get("QSTASH_CURRENT_SIGNING_KEY")!,
+    nextSigningKey: Deno.env.get("QSTASH_NEXT_SIGNING_KEY")!,
+  });
+
+  const isValid = await r.verify({
+    signature: req.headers.get("Upstash-Signature")!,
+    body: await req.text(),
+  }).catch((err: Error) => {
+    console.error(err);
+    return false;
+  });
+
+  if (!isValid) {
+    return new Response("Invalid signature", { status: 401 });
+  }
+
+  new Response("OK", { status: 200 });
+});
